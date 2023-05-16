77f1ad36b7853140b87eb7534c7e828e8420b673
Jimmy Adediran <jim_adediran@yahoo.com>
2023-05-16 19:42:04 -0400

    created ModelSerializer class ArticleSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Article
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=400)
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'description']
+   
