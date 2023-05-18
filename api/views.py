14fd34907e32338308df5755fc942a14bcade48e
Jimmy Adediran <jim_adediran@yahoo.com>
2023-05-18 12:14:38 -0400

    created a post funcitoin in article_list

--- api/views.py
+++ api/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, HttpResponse
 from .models import Article
 from .serializers import ArticleSerializer
 from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def article_list(request):
     
     #getallarticles
@@ -12,3 +15,11 @@ def article_list(request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+        
