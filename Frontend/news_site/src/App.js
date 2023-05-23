e7bd5867549d149b4b50666eb57965bb7d07f5a8
Jimmy Adediran <jim_adediran@yahoo.com>
2023-05-23 13:35:45 -0400

    created FunComponent.js returns text field

--- Frontend/news_site/src/App.js
+++ Frontend/news_site/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
+import FunComponent from './components/FunComponent';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React Application</h1>
+      <Hello/>
+      <FunComponent/>
     </div>
   );
 }
